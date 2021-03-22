75030b293aee09219a595e8290228190a195c6b3
Ritz GL <maurigarzonlara@hotmail.com>
2021-03-22 16:49:30 +0800

    implemented form for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var formHolder = document.getElementsByClassName("form-holder");
+// might be broken up into additinonal 4 functions
+// one function per criteria, then perhaps 
+// use switchcase to execute appropriate code block
+function generatePassword(){
+
+
+}
+
+// open new div in html document
+// display password criteria
+  // length 8 - 125 characters long
+  // include lowercase
+  // include uppercase
+  // include symbols 
+function insertCriteriaMenu(){
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
