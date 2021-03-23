2f0087d9ff674eeafff943fab820494d15f649df
Ritz GL <maurigarzonlara@hotmail.com>
2021-03-23 16:53:05 +0800

    code restructure to fit spec

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var formHolder = document.getElementsByClassName("form-holder");
-// might be broken up into additinonal 4 functions
-// one function per criteria, then perhaps 
-// use switchcase to execute appropriate code block
-function generatePassword(){
 
+// generate 4 different arrays, with the ASCII codes for each criteria
+var lowercase = generateArray(97,122);
+var uppercase = generateArray(65,90);
+var numbers = generateArray(48,57);
+var symbols = generateArray(33, 47).concat(
+  generateArray(58,64)
+).concat(
+  generateArray(91,96)
+).concat(
+  generateArray(123,126)
+);
+console.log(lowercase);
+console.log(uppercase);
+console.log(numbers);
+console.log(symbols);
 
+function generateArray(low, high){
+  var myArray = [];
+  for(var i = low; i <= high; i++){
+    myArray.push(i);
+  }
+  return myArray;
 }
 
-// open new div in html document
-// display password criteria
-  // length 8 - 125 characters long
-  // include lowercase
-  // include uppercase
-  // include symbols 
-function insertCriteriaMenu(){
+//write a function that generates random character from an array
+function randomlySelectSingleCharacter(array){
+  var i = Math.floor(Math.random() * (array.length));
+  
+  var randomCharacter = String.fromCharCode(array[i]);
 
-
+  return randomCharacter;
 }
+
+for(var i = 0; i < 100; i++){
+  var randomCharacter = randomlySelectSingleCharacter(lowercase);
+  console.log("random character generated from lowercase array: ", randomCharacter);
+}
+
+
+
+// write a function that concatenates the arrays depending on user response
+
+
+// write a function that generates a random string from appended arrays
+
+
+// write a function that concatenates the random string to random pasword
+
 
 // Write password to the #password input
 function writePassword() {
